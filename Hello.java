cbec8631623d265a9ecd2034d38526fbd81a8e34
E41221590_Muhammad ilham_firmansyah_B <125242657+Ilhamcom23@users.noreply.github.com>
2023-02-13 15:31:26 +0700

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,15 @@
+
+package hello;
+
+
+public class Hello {
+
+  
+    public static void main(String[] args) {
+       
+        System.out.println("hello word");
+        System.out.println("nama: muhammad  ilham firmansyah ");
+        System.out.println("nim : E41221590");
+    }
+    
+}
